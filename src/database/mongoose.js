843e78d5494c181e105b9c03d2927696f2319c55
sachins0412 <saxenas964@gmail.com>
2022-09-15 21:08:22 +0530

    set up mongoose connection

---
+++ src/database/mongoose.js
@@ -0,0 +1,3 @@
+const mongoose = require("mongoose");
+const mongoDBURI = "mongodb://127.0.0.1:27017/task-api-elred";
+mongoose.connect(mongoDBURI);
--- src/server.js
+++ src/server.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
+
+require("./database/mongoose");
 
 const userRouter = require("./router/userRouter");
 
-app.use("/user", userRouter);
+app.use("/users", userRouter);
 
 app.listen(3000, () => console.log("server up"));

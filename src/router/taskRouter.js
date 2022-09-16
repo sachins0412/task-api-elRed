5f2ee8e0a23934286e5eeff958460f2f819ea262
sachins0412 <saxenas964@gmail.com>
2022-09-16 21:09:30 +0530

    implement delete route for tasks

--- src/router/taskRouter.js
+++ src/router/taskRouter.js
@@ -32,4 +32,17 @@ router.patch("/:id", async (req, res) => {
   }
 });
 
+router.delete("/:id", async (req, res) => {
+  try {
+    const task = await Task.findByIdAndDelete(req.params.id);
+
+    if (!task) {
+      return res.status(404).send("task not found");
+    }
+
+    res.send(task);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
 module.exports = router;

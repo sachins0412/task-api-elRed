015748408d5f0f61144e09493de4a8a8233e82e6
sachins0412 <saxenas964@gmail.com>
2022-09-16 20:52:32 +0530

    add tasks router

--- src/server.js
+++ src/server.js
@@ -5,7 +5,10 @@ app.use(express.json());
 require("./database/mongoose");
 
 const userRouter = require("./router/userRouter");
+const taskRouter = require("./router/taskRouter");
 
 app.use("/users", userRouter);
+
+app.use("/tasks", taskRouter);
 
 app.listen(3000, () => console.log("server up"));

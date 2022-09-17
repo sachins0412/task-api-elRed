3689e09aceb69ae74e9cafd05308c8e1fe4f5343
sachins0412 <saxenas964@gmail.com>
2022-09-17 15:52:09 +0530

    implement auth and logout route

--- src/router/userRouter.js
+++ src/router/userRouter.js
@@ -7,7 +7,8 @@ router.post("/signup", async (req, res) => {
   const user = new User(req.body);
   try {
     await user.save();
-    res.send(user);
+    req.session.isAuth = true;
+    res.send("SignUp successful and you are logged in now");
   } catch (error) {
     res.status(400).send(error);
   }
@@ -16,11 +17,20 @@ router.post("/signup", async (req, res) => {
 router.post("/login", async (req, res) => {
   try {
     const user = await User.findUser(req.body.email, req.body.password);
+    req.session.isAuth = true;
     res.send(user);
   } catch (error) {
     error.status = error.status || 500;
     res.status(error.status).json(error);
   }
+});
+
+router.post("/logout", (req, res) => {
+  if (req.session.isAuth) {
+    req.session.destroy();
+    return res.send("logged off now");
+  }
+  res.status(401).send("you are not loggedin");
 });
 
 module.exports = router;

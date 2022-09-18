73a9277c1831b6bbf66788d32499f14405d450fb
sachins0412 <saxenas964@gmail.com>
2022-09-18 15:53:16 +0530

    call next

--- src/models/user.js
+++ src/models/user.js
@@ -40,6 +40,8 @@ userSchema.pre("save", async function (next) {
   if (user.isModified("password")) {
     user.password = await bcrypt.hash(user.password, 8);
   }
+
+  next();
 });
 
 userSchema.statics.findUser = async (email, password) => {

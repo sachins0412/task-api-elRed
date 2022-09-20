b160c1ed1f4c114332b93d0f7513a8f23af40170
sachins0412 <saxenas964@gmail.com>
2022-09-20 20:21:16 +0530

    change hard-coded URI to env var

--- src/middlewares/initializeSession.js
+++ src/middlewares/initializeSession.js
@@ -3,7 +3,7 @@ const session = require("express-session");
 const MongoDBSession = require("connect-mongodb-session")(session);
 
 const store = new MongoDBSession({
-  uri: "mongodb://127.0.0.1:27017/task-api-elred",
+  uri: process.env.MONGODB_URI,
   collection: "userSessions",
 });
 

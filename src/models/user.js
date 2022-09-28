cd9dc7ef9b39c57a3e00ec435d36bcac3f19ae41
sachins0412 <saxenas964@gmail.com>
2022-09-28 21:55:35 +0530

    set jwt expiration time to 30sec

--- src/models/user.js
+++ src/models/user.js
@@ -60,13 +60,16 @@ userSchema.methods.toJSON = function () {
   const user = this;
   const userObject = user.toObject();
   delete userObject.password;
+  delete userObject.tokens;
   delete userObject.otp;
   return userObject;
 };
 
 userSchema.methods.generateAuthToken = async function () {
   const user = this;
-  const token = jwt.sign({ _id: user._id.toString() }, process.env.JWT_SECRET);
+  const token = jwt.sign({ _id: user._id.toString() }, process.env.JWT_SECRET, {
+    expiresIn: 30,
+  });
   user.tokens = user.tokens.concat({ token });
   await user.save();
   return token;

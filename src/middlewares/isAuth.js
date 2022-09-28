a32fff26bdda7c4522222adcb572f254f6ca591b
sachins0412 <saxenas964@gmail.com>
2022-09-28 20:33:13 +0530

    authorize request using jwt

--- src/middlewares/isAuth.js
+++ src/middlewares/isAuth.js
@@ -1,5 +1,23 @@
-const isAuth = (req, res, next) => {
-  //will do this with jwt
+const jwt = require("jsonwebtoken");
+const { User } = require("./../models/user");
+
+const isAuth = async (req, res, next) => {
+  try {
+    const token = req.header("Authorization").replace("Bearer ", "");
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await User.findOne({
+      _id: decoded._id,
+      "tokens.token": token,
+    });
+    if (!user) {
+      throw new Error();
+    }
+    req.token = token;
+    req.user = user;
+    next();
+  } catch (e) {
+    res.status(401).send("User not authorized");
+  }
 };
 
 module.exports = isAuth;
